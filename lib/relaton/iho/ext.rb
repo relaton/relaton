e7798edc30ce165a1f8faeef03ac3941472939c1
Andrei Kislichenko <android.2net@gmail.com>
2026-02-19 16:30:29 -0500

    Refactor Ext class and update schema version in fixtures; remove unused attributes and adjust XML mappings

--- lib/relaton/iho/ext.rb
+++ lib/relaton/iho/ext.rb
@@ -3,21 +3,16 @@ require_relative "comment_period"
 
 module Relaton
   module Iho
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, :string
+    class Ext < Bib::Ext
       attribute :doctype, Doctype
-      attribute :subdoctype, :string
-      attribute :flavor, :string
-      attribute :ics, Bib::ICS, collection: true
       attribute :commentperiod, CommentPeriod
 
       xml do
-        map_attribute "schema-version", to: :schema_version
-        map_element "doctype", to: :doctype
-        map_element "subdoctype", to: :subdoctype
-        map_element "flavor", to: :flavor
-        map_element "ics", to: :ics
         map_element "commentperiod", to: :commentperiod
+      end
+
+      def schema_version
+        Relaton.schema_versions["relaton-model-iho"]
       end
     end
   end
