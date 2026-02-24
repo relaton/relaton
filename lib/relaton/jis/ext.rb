b7be86d0e9a0ab8cb6a2c755de6bd7835ec2330f
Andrei Kislichenko <android.2net@gmail.com>
2026-02-24 15:24:38 -0500

    Update grammar, Get rid of ext/editorialgroup relaton/relaton-bib#103

--- lib/relaton/jis/ext.rb
+++ lib/relaton/jis/ext.rb
@@ -2,23 +2,12 @@ require_relative "doctype"
 
 module Relaton
   module Jis
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, :string
+    class Ext < Iso::Ext
+      attribute :schema_version, method: :get_schema_version
       attribute :doctype, Doctype
-      attribute :editorialgroup, Iso::ISOProjectGroup
-      attribute :flavor, :string
-      attribute :ics, Bib::ICS, collection: true
-      attribute :structuredidentifier, Iso::StructuredIdentifier
-      attribute :stagename, Iso::Stagename
 
-      xml do
-        map_attribute "schema-version", to: :schema_version
-        map_element "doctype", to: :doctype
-        map_element "editorialgroup", to: :editorialgroup
-        map_element "flavor", to: :flavor
-        map_element "ics", to: :ics
-        map_element "structuredidentifier", to: :structuredidentifier
-        map_element "stagename", to: :stagename
+      def get_schema_version
+        Relaton.schema_versions["relaton-model-jis"]
       end
     end
   end
