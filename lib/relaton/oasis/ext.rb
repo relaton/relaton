6e0198b86ab3302ed0b333fa3f6d661afb554020
Andrei Kislichenko <android.2net@gmail.com>
2026-02-25 15:41:07 -0500

    Update grammars & Get rid of ext/editorialgroup relaton/relaton-bib#103

--- lib/relaton/oasis/ext.rb
+++ lib/relaton/oasis/ext.rb
@@ -2,13 +2,9 @@ require_relative "doctype"
 
 module Relaton
   module Oasis
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, :string
+    class Ext < Bib::Ext
+      attribute :schema_version, method: :get_schema_version
       attribute :doctype, Doctype
-      attribute :subdoctype, :string
-      attribute :editorialgroup, Bib::EditorialGroup
-      attribute :ics, Bib::ICS, collection: true
-      attribute :structuredidentifier, Bib::StructuredIdentifier, collection: true
       attribute :technology_area, :string, collection: true, values: %w[
         Cloud Content-Technologies Cybersecurity e-Business eGov/Legal Emergency-Management
         Energy Information-Modeling IoT Lifecycle-Integration Localization Messaging
@@ -16,13 +12,11 @@ module Relaton
       ]
 
       xml do
-        map_attribute "schema-version", to: :schema_version
-        map_element "doctype", to: :doctype
-        map_element "subdoctype", to: :subdoctype
-        map_element "editorialgroup", to: :editorialgroup
-        map_element "ics", to: :ics
-        map_element "structuredidentifier", to: :structuredidentifier
         map_element "technology-area", to: :technology_area
+      end
+
+      def get_schema_version
+        Relaton.schema_versions["relaton-model-oasis"]
       end
     end
   end
