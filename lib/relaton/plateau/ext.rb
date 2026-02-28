a24f707498c802bc809121cf35502c7cc12a01b3
Andrei Kislichenko <android.2net@gmail.com>
2026-02-27 19:48:23 -0500

    Update grammar & Get rid of ext/editorialgroup relaton/relaton-bib#103

--- lib/relaton/plateau/ext.rb
+++ lib/relaton/plateau/ext.rb
@@ -2,26 +2,19 @@ require_relative "doctype"
 
 module Relaton
   module Plateau
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, :string
+    class Ext < Bib::Ext
+      attribute :schema_version, method: :get_schema_version
       attribute :doctype, Doctype
-      attribute :subdoctype, :string
-      attribute :flavor, :string
-      attribute :editorialgroup, Iso::ISOProjectGroup
-      attribute :ics, Bib::ICS, collection: true
-      attribute :structuredidentifier, Bib::StructuredIdentifier
       attribute :stagename, Iso::Stagename
       attribute :filesize, :integer
 
       xml do
-        map_attribute "schema-version", to: :schema_version
-        map_element "doctype", to: :doctype
-        map_element "subdoctype", to: :subdoctype
-        map_element "flavor", to: :flavor
-        map_element "editorialgroup", to: :editorialgroup
-        map_element "ics", to: :ics
-        map_element "structuredidentifier", to: :structuredidentifier
         map_element "stagename", to: :stagename
+        map_element "filesize", to: :filesize
+      end
+
+      def get_schema_version
+        Relaton.schema_versions["relaton-model-plateau"]
       end
     end
   end
