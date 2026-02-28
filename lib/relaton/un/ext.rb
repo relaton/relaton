8c47d56da4473aec70ae967be5ced8b13cbb3352
Andrei Kislichenko <android.2net@gmail.com>
2026-02-27 20:50:28 -0500

    Refactor XML grammar definitions: remove editorial group, update schema version, and enhance documentation

--- lib/relaton/un/ext.rb
+++ lib/relaton/un/ext.rb
@@ -1,16 +1,12 @@
 require_relative "doctype"
-require_relative "editorial_group"
 require_relative "session"
 
 module Relaton
   module Un
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, :string
+    class Ext < Bib::Ext
+      attribute :schema_version, method: :get_schema_version
       attribute :doctype, Doctype
-      attribute :subdoctype, :string
       attribute :submissionlanguage, :string, collection: true
-      attribute :editorialgroup, EditorialGroup
-      attribute :ics, Bib::ICS, collection: true
       attribute :distribution, :string, values: %w[general limited restricted provisional]
       attribute :session, Session
       attribute :job_number, :string
@@ -20,11 +16,14 @@ module Relaton
         map_element "doctype", to: :doctype
         map_element "subdoctype", to: :subdoctype
         map_element "submissionlanguage", to: :submissionlanguage
-        map_element "editorialgroup", to: :editorialgroup
         map_element "ics", to: :ics
         map_element "distribution", to: :distribution
         map_element "session", to: :session
         map_element "job_number", to: :job_number
+      end
+
+      def get_schema_version
+        Relaton.schema_versions["relaton-model-un"]
       end
     end
   end
