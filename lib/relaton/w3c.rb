76f0e7183c54f15f03a1b46bd7b95208db22b115
Andrei Kislichenko <android.2net@gmail.com>
2026-03-04 22:31:55 -0500

    port Bibliography from v1

--- lib/relaton/w3c.rb
+++ lib/relaton/w3c.rb
@@ -5,18 +5,21 @@ require_relative "w3c/util"
 require_relative "w3c/item"
 require_relative "w3c/bibitem"
 require_relative "w3c/bibdata"
+require_relative "w3c/bibliography"
 
-module RelatonW3c
-  INDEXFILE = "index-v1".freeze
+module Relaton
+  module W3c
+    INDEXFILE = "index-v1".freeze
 
-  class Error < StandardError; end
+    class Error < StandardError; end
 
-  # Returns hash of XML reammar
-  # @return [String]
-  def self.grammar_hash
-    # gem_path = File.expand_path "..", __dir__
-    # grammars_path = File.join gem_path, "grammars", "*"
-    # grammars = Dir[grammars_path].sort.map { |gp| File.read gp }.join
-    Digest::MD5.hexdigest RelatonW3c::VERSION + RelatonBib::VERSION # grammars
+    # Returns hash of XML reammar
+    # @return [String]
+    def self.grammar_hash
+      # gem_path = File.expand_path "..", __dir__
+      # grammars_path = File.join gem_path, "grammars", "*"
+      # grammars = Dir[grammars_path].sort.map { |gp| File.read gp }.join
+      Digest::MD5.hexdigest Relaton::W3c::VERSION + Relaton::Bib::VERSION # grammars
+    end
   end
 end
