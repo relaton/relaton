b2a4f20c27400241d6fc6103b7e323a6c6d73d06
Andrei Kislichenko <android.2net@gmail.com>
2026-04-09 18:23:16 -0400

    Tests optimization
    Each integration test loads and parses index file.
    
    This slows down the tests and creates huge VCRs

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,6 +1,23 @@
 require "webmock/rspec"
+require "zip"
+require "yaml"
+
+INDEX_ZIP_PATH = File.join(__dir__, "..", "fixtures", "index-v1.zip")
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    yaml = Zip::File.open(INDEX_ZIP_PATH) do |zip|
+      zip.first.get_input_stream.read
+    end
+    index_data = YAML.safe_load(yaml, permitted_classes: [Symbol])
+
+    type = Relaton::Index::Type.new(:iana, nil, "index-v1.yaml")
+    type.instance_variable_set(:@index, index_data)
+    type.define_singleton_method(:actual?) { |**args| args.key?(:url) }
+
+    Relaton::Index.pool.instance_variable_get(:@pool)[:IANA] = type
+  end
+
   config.before(:each) do
     WebMock.reset!
     WebMock.disable_net_connect!
