f700d00fbdd2f17814d7914fb375bf16eea27694
Andrei Kislichenko <android.2net@gmail.com>
2026-05-02 14:31:34 -0400

    Upd lutaml model to 0.8.0 (#35)
    * Update lutaml-model and refactor BSI models for compatibility with lutaml-model 0.8.0
    
    * Update VCRs

--- lib/relaton/bsi/model/bibdata.rb
+++ lib/relaton/bsi/model/bibdata.rb
@@ -1,6 +1,7 @@
 module Relaton
   module Bsi
     class Bibdata < Item
+      model ItemData
       include Bib::BibdataShared
     end
   end
--- lib/relaton/bsi/model/ext.rb
+++ lib/relaton/bsi/model/ext.rb
@@ -2,28 +2,16 @@ require_relative "doctype"
 
 module Relaton
   module Bsi
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, method: :get_schema_version
+    class Ext < Bib::Ext
       attribute :doctype, Doctype
-      attribute :subdoctype, :string
-      attribute :flavor, :string
       attribute :ics, Bib::ICS, collection: true
       attribute :structuredidentifier, Iso::StructuredIdentifier, collection: true, initialize_empty: true
       attribute :stagename, Iso::Stagename
 
-      xml do
-        map_attribute "schema-version", to: :schema_version
-        map_element "doctype", to: :doctype
-        map_element "subdoctype", to: :subdoctype
-        map_element "flavor", to: :flavor
-        map_element "ics", to: :ics
-        map_element "stagename", to: :stagename
-        map_element "structuredidentifier", to: :structuredidentifier
-      end
+      xml { map_element "stagename", to: :stagename }
+      key_value { map_element "stagename", to: :stagename }
 
-      def get_schema_version
-        Relaton.schema_versions["relaton-model-bsi"]
-      end
+      def get_schema_version = Relaton.schema_versions["relaton-model-bsi"]
     end
   end
 end
