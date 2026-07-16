5c9370f9ab0cdc027283be05fa125c74caef2f6e
Andrei Kislichenko <android.2net@gmail.com>
2026-07-15 23:43:34 -0400

    test(jcgm): cover JCGM processor routing in registry + CLI fetch specs (#61)

--- gems/relaton-cli/spec/acceptance/relaton_fetch_spec.rb
+++ gems/relaton-cli/spec/acceptance/relaton_fetch_spec.rb
@@ -25,6 +25,13 @@ RSpec.describe "Relaton Fetch" do
         Relaton::Cli.start(command)
       end
 
+      it "calls fetch for JCGM" do
+        expect(db).to receive(:fetch_std).with "JCGM 100:2008", nil, :relaton_jcgm, type: "JCGM"
+
+        command = ["fetch", "--type", "JCGM", "JCGM 100:2008"]
+        Relaton::Cli.start(command)
+      end
+
       it "ignore cache" do
         expect(db).to receive(:fetch).with "ISO 2146", nil, no_cache: true
 
